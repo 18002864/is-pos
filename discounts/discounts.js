f9ec6220e2cbc40b8a039a39c4594d705019f0a3
Daniel Sierra <daniel.sierra@ubiquo.io>
2020-10-23 08:59:44 -0600

    asdnasjkd

--- discounts/discounts.js
+++ discounts/discounts.js
@@ -33,7 +33,7 @@ const getDiscountsById = (request, response) => {
 }
 
 const createDiscounts = (request, response) => {
-	console.log('body -------->', request.body)
+	console.log('body -------->', request)
     const { id_bodega, sku, starts, ends } = request.body
 	// yyyy mm dd
 	console.log('id bodega --------------->', id_bodega)
