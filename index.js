675bfaa9b63b7039284e824dda3b176cc5481d2b
16003303@galileo.edu <16003303@galileo.edu>
2020-10-29 17:34:41 -0600

    generar pdf

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 const { Pool } = require('pg')
 const PORT = process.env.PORT || 3000;
+const { createInvoice } = require("./invoicePdf/createInvoice.js");
 
 const products = require('./products/products')
 const stores = require('./stores/stores')
@@ -47,6 +48,7 @@ app.get('/sales/customer/:id', sales.getSalesByCustomerId)
 app.post('/sales', sales.createSales)
 app.delete('/sales/:id', sales.deleteSale)
 app.put('/pointOfSasalesles/:id', sales.updateSales)
+
 app.get('/pos/2/external-sales/:external_sale_id', sales.getExternalSales)
 app.post('/pos/2/external-sales', (request, response) => {
 
@@ -127,53 +129,109 @@ app.post('/pos/2/external-sales', (request, response) => {
       (id_bodega, massive_sale_id, customer_id, total_sale, total_discount,
       created_at, created_by, external_sale_id, invoice_id) 
       values($1,$2,$3,$4,$5,now() at time zone 'America/Guatemala',$6,$7,$8) RETURNING id_sale`,
-    [
-      id_bodega, massive_sale_id, customer_id, total_sale, total_discount,
-      created_by,external_sale_id, invoice_id
-    ],
+      [
+        id_bodega, massive_sale_id, customer_id, total_sale, total_discount,
+        created_by, external_sale_id, invoice_id
+      ],
 
-    (error, sales_result) => {
-      if (error) {
-        response.status(500).send('Something went wrong were sorry')
-      } else {
-        if (sales_result.rows.length > 0) {
-          products.map((item) => {
-            axios({
-              method: 'post',
-              url: 'https://inventarios-is.herokuapp.com/bodega/7/out',
-              data: {
-                sku: item.product_code,
-                quantity: item.quantity
-              }
-            }).then((externalApiResponse) => {
-              if (externalApiResponse.status === 200) {
-                pool.query(
-                  `insert into sales_products
+      (error, sales_result) => {
+        if (error) {
+          response.status(500).send('Something went wrong were sorry')
+        } else {
+          if (sales_result.rows.length > 0) {
+            products.map((item) => {
+              axios({
+                method: 'post',
+                url: 'https://inventarios-is.herokuapp.com/bodega/7/out',
+                data: {
+                  sku: item.product_code,
+                  quantity: item.quantity
+                }
+              }).then((externalApiResponse) => {
+                if (externalApiResponse.status === 200) {
+                  pool.query(
+                    `insert into sales_products
                 (
                   id_sale, id_bodega, sku, quantity, unit_price,
                   discount_percentage, total_product, 
                   total_discount, total, created_at, created_by
                 )
                 values($1,$2,$3,$4,$5,$6,$7,$8,$9,Now(),$10)`,
-                  [sales_result.rows[0].id_sale, id_bodega, item.product_code, item.quantity,
-                  item.unit_price, item.discount_percentage, item.total_product,
-                  item.total_discount, item.total, created_by
-                  ],
-                  (error, sales_products_reslt) => {
-                    if (error) {
-                      response.status(500).send('Algo exploto')
-                    }
-                  })
-                response.send({ id: sales_result.rows[0].id_sale })
-              }
+                    [sales_result.rows[0].id_sale, id_bodega, item.product_code, item.quantity,
+                    item.unit_price, item.discount_percentage, item.total_product,
+                    item.total_discount, item.total, created_by
+                    ],
+                    (error, sales_products_reslt) => {
+                      if (error) {
+                        response.status(500).send('Algo exploto')
+                      }
+                    })
+                  response.send({ id: sales_result.rows[0].id_sale })
+                }
+              })
             })
-          })
+          }
         }
-      }
-    })
+      })
   }
 })
+app.get('/pos/2/external-sales/:external_sale_id/invoice', (request, response) => {
+  const id = request.params.external_sale_id;
+  const pool = new Pool({
+    user: 'postgres',
+    host: 'is-pos.cpuskbampv3u.us-east-2.rds.amazonaws.com',
+    database: 'pos',
+    password: 'manchasymima',
+    port: 5432,
+  })
+  pool.query(`
+	select massive_sale_id, external_sale_id, customer_id, invoice_id,
+        total_sale, total_discount
+            from sales
+            where external_sale_id = $1`, [id],
+    (error, results) => {
+      if (error) {
+        console.log('error', error)
+        throw error
+      }
+      let body = results.rows
 
+      pool.query(`
+            select id_sale from sales where external_sale_id = $1;`, [id],
+        (error2, results2) => {
+          if (error2) {
+            console.log('error', error2)
+            throw error2
+          }
+          let id_sale = results2.rows[0].id_sale
+
+          pool.query(`
+            select sku product_code, quantity, unit_price, discount_percentage,
+                total_product, total_discount, total
+                from sales_products
+                    where id_sale = $1 ;`, [id_sale],
+            (error3, results3) => {
+              if (error3) {
+                console.log('error', error3)
+                throw error3
+              }
+              body[0].products = results3.rows
+              axios({
+                method: 'get',
+                url: 'https://ids-crm.herokuapp.com/api/costumer/read_single.php?nit=' + body[0].customer_id,
+              }).then(res => {
+                let info = res.data;
+                if (info.nit != null) {
+                  body[0].nombres = info.cname;
+                  body[0].direccion = info.caddress;
+                  body[0].email = info.cemail + ", " + info.cphone;;
+                  createInvoice(body[0], response);
+                }
+              })
+            })
+        })
+    })
+})
 
 // https://ids-crm.herokuapp.com/api/costumer/create.php
 
@@ -218,3 +276,7 @@ app.post('/auth', (request, response) => {
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}.`)
 })
+
+
+
+
