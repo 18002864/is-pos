29d0fa83b764db01c386580f34090ea61aadcfc1
Daniel Sierra <daniel.sierra@ubiquo.io>
2020-10-28 23:42:24 -0600

    new auth to external sales

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/sales/customer/:id', sales.getSalesByCustomerId)
 app.post('/sales', sales.createSales)
 app.delete('/sales/:id', sales.deleteSale)
 app.put('/pointOfSasalesles/:id', sales.updateSales)
-app.post('/pos/2/external', (request, response) => {
+app.post('/pos/2/external-sales', (request, response) => {
 
   const pool = new Pool({
     user: 'postgres',
