e0aca3f8342033c826710b4a843b4ed2dcfb2816
Daniel Sierra <daniel.sierra@ubiquo.io>
2020-10-29 08:20:54 -0600

    listo viejo

--- sales/sales.js
+++ sales/sales.js
@@ -96,7 +96,7 @@ const getSalesByCustomerId = (request, response) => {
 		if (error) {
 			throw error
 		}
-		response.status(200).json(results.rows[0])
+		response.status(200).json(results.rows)
 	})
 }
 
