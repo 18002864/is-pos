b17e3ad31856c29f40e86665d31d7375bc2580cf
16003303@galileo.edu <16003303@galileo.edu>
2020-10-30 14:44:29 -0600

    cambios

--- discounts/discounts.js
+++ discounts/discounts.js
@@ -101,7 +101,7 @@ const getDiscountsByActiveProduct = (request, response) => {
 	
 	pool.query(`
 		select sku, COALESCE (max(discount), 0 , max(discount)) as discount from product_discount
-		  where id_bodega = $1 and sku = $2 and now() between starts and ends+1 group by sku`, 
+		  where id_bodega = $1 and sku = $2 and now() at time zone 'America/Guatemala' between starts and ends+1 group by sku`, 
 		  [id_bodega, sku], (error, results) => {
 		if (error) {
 			throw error
